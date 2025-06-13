2ab33b2b5f4d1095d28f6644a83302edb9afdda9
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-13 16:04:54 +0200

    Refactor array iteration methods and add functionality to filter even numbers

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const authorsName = [];
+posts.map((post) => authorsName.push(post.author));
+console.log(authorsName);
